935230a15413a39c11f00530c263d6efe96b07ae
Gaurav <gauravara55@gmail.com>
2025-08-30 20:36:14 +0530

    Main && SmartKitchen

--- src/Main.java
+++ src/Main.java
@@ -102,6 +102,20 @@ public class Main {
 //        thePC.getMotherboard().loadProgram("Windows OS");
 //        thePC.getComputerCase().pressPowerButton();
         thePC.powerUp();
+
+//        --------------Kitchen Practice -------------------
+        SmartKitchen kitchen = new SmartKitchen();
+
+//        kitchen.getDishWasher().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//
+//        kitchen.getDishWasher().doDishes();
+//        kitchen.getIceBox().orderFood();
+//        kitchen.getBrewMaster().brewCoffee();
+
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
     }
 
     public static void printInformation(String string) {
