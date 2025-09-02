0bbf10d05a44c0a2f0f246dff4ff59986ef8aed3
Gaurav <gauravara55@gmail.com>
2025-09-02 14:56:30 +0530

    Contact+MobilePhone+MainRunContact added

---
+++ src/Contact.java
@@ -0,0 +1,36 @@
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Factory method (optional)
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Contact)) return false;
+        Contact contact = (Contact) obj;
+        return this.name.equals(contact.name); // equality by name only
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
+
