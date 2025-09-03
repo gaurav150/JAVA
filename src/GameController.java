df6db67246877492a85408e5b22f8508f9b117f9
Gaurav <gauravara55@gmail.com>
2025-09-03 20:05:15 +0530

    FiveCardDrawPokerGame

---
+++ src/GameController.java
@@ -0,0 +1,9 @@
+public class GameController {
+
+
+
+    public static void main(String[] args) {
+        PokerGame fiveCardDraw = new PokerGame(8,5);
+        fiveCardDraw.startPlay();
+    }
+}
---
+++ src/Ranking.java
@@ -0,0 +1,8 @@
+public enum Ranking {
+    NONE, ONE_PAIR, TW0_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND;
+
+    @Override
+    public String toString() {
+        return this.name().replace('_', ' ');
+    }
+}
