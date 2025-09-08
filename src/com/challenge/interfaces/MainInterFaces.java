73a8574e8548b01df0d1129b791b6683fad9b0c7
Gaurav <gauravara55@gmail.com>
2025-09-08 09:25:20 +0530

    deleted commented lines

--- src/com/challenge/interfaces/MainInterFaces.java
+++ src/com/challenge/interfaces/MainInterFaces.java
@@ -10,8 +10,6 @@ public class MainInterFaces {
         FlightEnabled flier = bird;
         Trackable tracked = bird;
         animal.move();
-//        flier.move();
-//        tracked.move();
         flier.fly();
         tracked.track();
         flier.land();
